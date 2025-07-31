03c07b19fdf7d588dcb55f1413d8d9567ea5e003
Ariel <arielfloressantacruz3@gmail.com>
2025-07-30 21:54:45 -0600

    Ariel:singleton

---
+++ patrones de diseño/introduccion.py
@@ -0,0 +1,33 @@
+# Hay tres categorias de patrones de diseño.
+# Creational Patterns: te permite crear de forma sencilla la creacion de objetos 
+#structural patter: mejoran la composicion y relacion entre los objetos
+# Behavior patter: ayudan a mejorar la comunicacion entre los objetos y la responsabilidad que le competen
+
+"Principales beneficios"
+# son soluciones probadas y ayudan a mejorar la solucion entre los desarolladores
+
+"Outline"
+
+"--> Creational Patterns"
+# Singleton: Un solo objeto para una clase
+# Factory Method: Crear objetos sin exponer logica de creacion
+# Builder: Contruccion de un objeto complejo paso a paso
+# Prototype: copiar objetos existentes
+# Abstract Factory: Crear familias de objetos relacionados
+
+"--> Structural Patters"
+# Adapter: Interfaz unificada entre interfaces incompatibles.
+# Decorator: Añadir responsabilidades a objetos dinamicamente.
+# Facade: Interfaz simplificada de un sistema complejo.
+# composite: Componer objetos en estructuras de arbol.
+# flyweight: Minimizar uso de memoria o costos computacionales.
+# Bridge: separar abstracciones de implementacion.
+
+"--> Behavior Patters"
+
+# Observer: suscribir y notificar cambios a varios objetos.
+# command: Encapsular una solicitud como un objeto.
+# Strategy: permitir la seleccion de un algoritmo en tiempo de ejecucion.
+# state: cambios de comportamiento de un objeto cuando su estado interno cambia.
+# Chan Of Responsibility: Pasar soluciones a lo largo de una cadena de manejadores.
+
