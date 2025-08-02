a4efea1633d1e1f67f7a6e624c9a4537d77149c8
Ariel <arielfloressantacruz3@gmail.com>
2025-08-02 17:23:50 -0600

    Ariel: Project1

---
+++ mlops/project1/src/data_colection.py
@@ -0,0 +1,15 @@
+import numpy as np
+import pandas as pd
+from  sklearn.model_selection import train_test_split
+import os
+
+data:pd.DataFrame = pd.read_csv("C:/Users/Ariel/Downloads/water_potability.csv")
+
+train_data, test_data  = train_test_split(data, test_size = 0.2, random_state = 42 )
+
+data_path = os.path.join('data', 'raw')
+
+os.makedirs(data_path)
+
+train_data.to_csv(os.path.join(data_path,'train.csv'), index = False)
+test_data.to_csv(os.path.join(data_path,'test.csv'), index = False)
